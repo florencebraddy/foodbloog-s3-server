4243cac2311b6bad5d6cff2eddc30ad49d5cbe0b
Florence Braddy <florencebraddy@hotmail.com>
2020-07-27 21:59:43 -0400

    setCredentials and getS3Data functions added

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 require("dotenv").config();
+const aws = require("aws-sdk");
 
 console.log(process.env.AWS_ACCESS_KEY);
 console.log(process.env.AWS_SECRET_ACCESS_KEY);
+
+function setCredentials() {
+  aws.config.setPromisesDependency();
+  aws.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: "us-east-2"
+  });
+}
+setCredentials();
+
+const s3 = new aws.S3();
+
+async function getS3Data() {
+  const s3response = await s3
+    .listObjectsV2({
+      Bucket: "recipestorage83411-dev",
+      Prefix: ""
+    })
+    .promise();
+
+  console.log(s3response);
+}
+getS3Data();
